eaee8bafd9ed76f9879402c0828bd26f4d7f1380
gianscarpe <gianluca@scarpellini.dev>
2020-12-04 14:33:24 +0100

    fixes for installation

--- cov2emacs/setup.py
+++ cov2emacs/setup.py
@@ -1,14 +1,23 @@
 # Copyright (c) 2009 Matt Harrison
 #from distutils.core import setup
+import re
 from setuptools import setup
 
-from cov2emacslib import meta
+def get_attr(attr: str):
+    version = re.search(  # type: ignore
+        r'^__version__\s*=\s*"(.*)"',
+        open("cov2emacslib/meta.py").read(),
+        re.MULTILINE,
+    ).group(1)
+    return version
+
+version = 0.1
 
 setup(name='cov2emacs',
-      version=meta.__version__,
-      author=meta.__author__,
+      version=version,
       description='FILL IN',
       scripts=['bin/cov2emacs'],
-      package_dir={'cov2emacslib':'cov2emacslib'},
+      package_dir={'cov2emacslib': 'cov2emacslib'},
       packages=['cov2emacslib'],
+      install_requires=['coverage']
 )
